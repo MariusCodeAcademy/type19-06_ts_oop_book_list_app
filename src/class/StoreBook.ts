2cdc37ca65aaaa856c2a006421211be9700e4347
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-14 11:23:07 +0200

    StoreBooks

---
+++ src/class/StoreBook.ts
@@ -0,0 +1,17 @@
+import { BookInterface } from '../interface/interface.js';
+
+export default class StoreBooks {
+  private static storageKey: string = 'MyBooks';
+
+  public static getData(): BookInterface[] {
+    const gotFromStorage = localStorage.getItem(StoreBooks.storageKey);
+    if (!gotFromStorage) return [];
+
+    // atversti is json i masyva
+    return JSON.parse(gotFromStorage);
+  }
+
+  public static saveData(data: BookInterface[]) {
+    localStorage.setItem(StoreBooks.storageKey, JSON.stringify(data));
+  }
+}
